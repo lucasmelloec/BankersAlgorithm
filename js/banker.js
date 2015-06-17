3d1d41a211de561167cea400febc836574989b33
Henrique Cintra Miranda <henrique.aranha@usp.br>
2015-06-17 02:24:50 +0000

    Algoritmo do banqueiro com estados

--- js/banker.js
+++ js/banker.js
@@ -23,11 +23,11 @@ Matrix.prototype = {
         this.elements.forEach( function( row ) { row.push( 0 ) } );
     },
     rowElementsLessEqualThan: function( array ) {
-        let i, test = true;
+        var i, test = true;
         for ( i = 0; i < this.elements.length && test == false; i++ )
         {
             test = true;
-            for ( let j = 0; j < this.elements[i].length && test; j++ )
+            for ( var j = 0; j < this.elements[i].length && test; j++ )
                 if ( this.elements[i][j] > array[j] )
                     test = false;
         }
@@ -37,7 +37,7 @@ Matrix.prototype = {
         this.elements[row][column] = value;
     },
     clone: function() {
-        let i, newMatrix = new Matrix (
+        var i, newMatrix = new Matrix (
             this.names.splice(), 
             this.vHeaders.splice(),
             this.hHeaders.splice()
@@ -49,17 +49,18 @@ Matrix.prototype = {
 };
 
 // Classe para salvar o estado do algoritmo do banqueiro
-function BankerState( E, P, A, allocationMatrix, demandMatrix ) {
+function BankerState( E, P, A, allocationMatrix, demandMatrix, process ) {
   this.E = E;
   this.P = P;
   this.A = A;
   this.allocationMatrix = allocationMatrix;
   this.demandMatrix = demandMatrix;
+  this.process = process;
 }
 
 BankerState.prototype = {
     clone: function() {
-        return new BankerState( this.E.splice(), this.P.splice(), this.A.splice(), this.allocationMatrix.clone(), this.demandMatrix.clone() );
+        return new BankerState( this.E.splice(), this.P.splice(), this.A.splice(), this.allocationMatrix.clone(), this.demandMatrix.clone(), this.process );
     }
 };
 
@@ -69,7 +70,7 @@ BankerState.prototype = {
 function banker()
 {
     // TODO: Visualizar processos concluídos. (Atualmente são apenas removidos da matriz)
-    let names = [ 'Processos', 'Recursos' ],
+    var names = [ 'Processos', 'Recursos' ],
         vHeaders = [ 'A', 'B', 'C', 'D' ], // 4 processos.
         hHeaders = [ 'Impressora', 'Scanner', 'CD-ROM', 'Disquete' ], // 4 recursos.
         demandMatrix = Matrix( names, vHeaders, hHeaders ), // Matriz que indica quantos recursos de cada tipo o processo irá pedir durante sua execução.
@@ -77,7 +78,9 @@ function banker()
         E = [ 5, 2, 10, 2 ], // Recursos existentes, ler do usuário.
         P = Array( E.length ), // Recursos sobre posse, definir pela matriz de alocação.
         A, // Recursos disponíveis, (E - P)
-        process, i, j;
+        current,
+        stateStack = [],
+        i;
         
         
         /* TODO: Ler os valores dos elementos da matriz de demanda e de alocação 
@@ -87,7 +90,7 @@ function banker()
         // Inicialização de P a partir da matriz de alocação
         P.forEach( function( element ) { element = 0 } );
         P.forEach( function( element ) {
-            for ( j = 0; j < allocationMatrix.elements[i].length; j++ )
+            for ( var j = 0; j < allocationMatrix.elements[i].length; j++ )
                 for ( i = 0; i < allocationMatrix.elements.length; i++ )
                     element += allocationMatrix.elements[i][j];
         } );
@@ -96,18 +99,36 @@ function banker()
         for ( i = 0; i < E.length; i++ )
             A.push( E[i] - P[i] );
 
+        // Estado atual, para ser clonado a cada passo
+        current = new BankerState( E, P, A, allocationMatrix, demandMatrix, E.length);
+
     while ( allocationMatrix.elements.length != 0 )
     {
-        process = demandMatrix.rowElementsLessEqualThan( A ); // Passo 1
-        if ( process != demandMatrix.elements.length )
+        // Passo 1
+        current.process = demandMatrix.rowElementsLessEqualThan( A );
+        // TODO: Desenhar seta para indicar current.processo selecionado ?
+        if ( current.process != demandMatrix.elements.length )
         {
-            for( let j = 0; j < A.length; j++ ) // Passo 2
+            // Passo 2 
+            // Fornece todos os recursos de que o processo necessita
+            for ( i = 0; i < A.length; i++ )
             {
-                A[j] += allocationMatrix.elements[process][j];
-                P[j] -= demandMatrix.elements[process][j] 
-                        + allocationMatrix[process][j];
+                A[i] -= demandMatrix.elements[current.process][i] - allocationMatrix.elements[current.process][i];
+                P[i] = E[i] - A[i];
             }
-            allocationMatrix.elements.splice( process, 1 );
+            allocationMatrix.elements[current.process] = demandMatrix.elements[current.process];
+            stateStack.push( current.clone() );
+            // TODO: Exibir valores de A, P e matriz de alocação atualizados
+            
+            // Processo se conclui, recursos são liberados
+            for( i = 0; i < A.length; i++ )
+            {
+                A[i] += allocationMatrix.elements[current.process][i];
+                P[i] = E[i] - A[i];
+            }
+            stateStack.push( current.clone() );
+            allocationMatrix.elements.splice( current.process, 1 );
+            //TODO: Exibir valores atualizados,
         }
         else
             return false;
