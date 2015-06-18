de2194cc62a864172b5a856f90f324c17761b0ff
Henrique Cintra Miranda <henrique.aranha@usp.br>
2015-06-18 19:11:45 +0000

    Corrigidos bugs relativos ao funcionamento básico do banqueiro.

--- js/banker.js
+++ js/banker.js
@@ -1,16 +1,16 @@
 var allocationMatrix = [];
 var demandMatrix = [[3, 1, 3], [5, 1, 0], [3, 1, 2]]; // Matriz que indica quantos recursos de cada tipo o processo irá pedir durante sua execução.
-
-var terminated = [ true, true, true ];
-
+var terminated = {
+    check: [ false, false, false ], // True indica que o processo foi concluído
+    count: 0 // Contagem de processos concluídos
+};
 var P = [];
 var A = [];
-var E = [ 5, 2, 3 ]; // Recursos existentes.
+var E = [5, 2, 3]; // Recursos existentes.
 
 var stateStack = [];
 
 function banker() {
-    var test;
     var current_process;
     
     // Inicialização de P a partir da matriz de alocação
@@ -22,37 +22,36 @@ function banker() {
             P[j] += allocationMatrix[i][j];
         }
     }
-        
+
     // Inicialização de A a partir de E e P
     for ( i = 0; i < E.length; i++ ) {
         A.push( E[i] - P[i] );
     }
 
-    while ( allocationMatrix.length != 0 ) {
+    while ( terminated.count != allocationMatrix.length ) {
         // Passo 1
-        current_process = null;
         for ( i = 0; i < allocationMatrix.length; i++ ) {
-            test = true;
-            for ( j = 0; j < allocationMatrix[0].length; j++ ) {
-                if ( terminated[i] == true && demandMatrix[i][j] - allocationMatrix[i][j] > A[j] ) {
-                    test = false;
-                    j = allocationMatrix[0].length;
+            if ( terminated.check[i] != true ) {
+                for ( j = 0; j < allocationMatrix[0].length; j++ ) {
+                    if ( demandMatrix[i][j] - allocationMatrix[i][j] > A[j] ) {
+                        j = allocationMatrix[0].length + 1;
+                    }
                 }
-            }
-            if ( test == true ) {
-                current_process = i;
-                i = allocationMatrix.length;
+                if ( j == allocationMatrix[0].length ) {
+                    current_process = i;
+                    i = allocationMatrix.length;
+                }
             }
         }
 
-        if ( current_process != null ) {
+        if ( terminated.check[ current_process ] == false ) {
             // Passo 2 
             // Fornece todos os recursos de que o processo necessita
             for ( i = 0; i < A.length; i++ ) {
                 A[i] -= demandMatrix[current_process][i] - allocationMatrix[current_process][i];
                 P[i] = E[i] - A[i];
             }
-            allocationMatrix[current_process] = demandMatrix[current_process];
+            allocationMatrix[current_process] = demandMatrix[current_process].splice();
             
             stateStack.push({
                 P: P,
@@ -66,8 +65,14 @@ function banker() {
                 A[i] += allocationMatrix[current_process][i];
                 P[i] = E[i] - A[i];
             }
-            
-            allocationMatrix.check[ current_process ] = false;
+            allocationMatrix[current_process].forEach(
+                function( element ) {
+                    element = 0;
+                }
+            );
+
+            terminated.check[current_process] = true;
+            terminated.count++;
             stateStack.push({
                 P: P,
                 A: A,
